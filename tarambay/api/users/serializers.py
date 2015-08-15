078dc819e67f3bf59f9cc6d4d9a73db5dfebb703
aida <acsdelosreyes@softwarelab7.com>
2015-08-16 00:50:59 +0800

    edits to UserSerializer, added ProfileSerializer

--- tarambay/api/users/serializers.py
+++ tarambay/api/users/serializers.py
@@ -8,7 +8,20 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'self', 'username', 'email', 'is_staff')
+        fields = ('id', 'self', 'username',)
+        extra_kwargs = {
+            'self': {'lookup_field': 'uuid'}
+        }
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.UUIDField(source='uuid', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'self', 'username', 'first_name', 'last_name', 'email',
+                  'is_staff')
+        read_only_fields = ('is_staff',)
         extra_kwargs = {
             'self': {'lookup_field': 'uuid'}
         }
