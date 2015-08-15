89e0c4f0dca0c43cf914645fe28f2586ff9901a1
aida <acsdelosreyes@softwarelab7.com>
2015-08-15 18:41:55 +0800

    added CategoryViewSet to api urls

--- tarambay/api/urls.py
+++ tarambay/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
+from api.events.views import CategoryViewSet
 from api.users.views import UserViewSet
 
 
 router = routers.DefaultRouter()
+router.register('categories', CategoryViewSet)
 router.register('users', UserViewSet)
 
 
