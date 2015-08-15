5083d2e162a519b8ce4d09ba1ef22902914cfb1a
aida <acsdelosreyes@softwarelab7.com>
2015-08-16 01:27:12 +0800

    added IsAnonUser permission

--- tarambay/api/permissions.py
+++ tarambay/api/permissions.py
@@ -23,3 +23,11 @@ class EventPermission(permissions.BasePermission):
 class IsObjectUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsAnonUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated():
+            return False
+        else:
+            return True
