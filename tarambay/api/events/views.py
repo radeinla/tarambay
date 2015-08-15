0423cfc1811ca3f690e80ece18fd5b4a56353690
aida <acsdelosreyes@softwarelab7.com>
2015-08-15 21:52:43 +0800

    use CreateEventSerialize for editing in EventViewSet, EventPermission for EventViewSet

--- tarambay/api/events/views.py
+++ tarambay/api/events/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, viewsets
 
-from .serializers import CategorySerializer, EventSerializer
+from .serializers import CategorySerializer, CreateEventSerializer, EventSerializer
+from api.permissions import EventPermission
 from tarambay.events.models import Category, Event
 
 
@@ -14,4 +15,13 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    create_serializer_class = CreateEventSerializer
     lookup_field = 'uuid'
+    permission_classes = (EventPermission,)
+
+    def get_serializer_class(self):
+        ACTIONS = ['create', 'update', 'partial_update']
+        if self.action in ACTIONS:
+            return self.create_serializer_class
+        else:
+            return self.serializer_class
