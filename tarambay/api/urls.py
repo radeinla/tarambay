06573e03a44df0f3985183fef23907d8926d7249
aida <acsdelosreyes@softwarelab7.com>
2015-08-16 00:52:25 +0800

    changed UserViewSet to read only, added ProfileView

--- tarambay/api/urls.py
+++ tarambay/api/urls.py
@@ -2,16 +2,18 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 from api.events.views import CategoryViewSet, EventViewSet
-from api.users.views import UserViewSet
+from api.users.views import ProfileView, UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register('categories', CategoryViewSet)
 router.register('events', EventViewSet)
+# router.register('users/profile', ProfileViewSet)
 router.register('users', UserViewSet)
 
 
 urlpatterns = [
+    url(r'^users/profile', ProfileView.as_view()),
     url(r'^', include(router.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
--- tarambay/api/users/views.py
+++ tarambay/api/users/views.py
@@ -1,10 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import generics, permissions, viewsets
 
-from .serializers import UserSerializer
+from .serializers import ProfileSerializer, UserSerializer
+from api.permissions import IsObjectUser
 from tarambay.users.models import User
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'uuid'
+    permission_classes = (permissions.AllowAny,)
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.none()
+    serializer_class = ProfileSerializer
+    lookup_field = 'uuid'
+    permission_class = (IsObjectUser,)
+
+    def get_object(self):
+        return self.request.user
