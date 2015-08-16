c48da0b055977cac4ff0740db372e3b2d7528e18
aida <acsdelosreyes@softwarelab7.com>
2015-08-16 13:12:08 +0800

    EventInviteView to invite people to events

--- tarambay/api/events/views.py
+++ tarambay/api/events/views.py
@@ -1,7 +1,8 @@
 import datetime
 import pytz
 from django.db.models import Q
-from rest_framework import permissions, viewsets
+from rest_framework import generics, permissions, viewsets
+from rest_framework.response import Response
 
 from .serializers import (CategorySerializer, CreateEventSerializer,
                           EventSerializer, InviteEventSerializer)
@@ -35,6 +36,26 @@ class EventViewSet(viewsets.ModelViewSet):
         now = datetime.datetime.now(pytz.utc)
         queryset = Event.objects.filter(start__gt=now)
         if user.is_authenticated():
-            return queryset.filter(Q(private=False) | Q(invited__user=user) | Q(admin=user)).order_by('-end', '-start')
+            return queryset.filter(Q(private=False) | Q(invited__user=user) | Q(admin=user)).distinct().order_by('-end', '-start')
         else:
-            return queryset.filter(private=False).order_by('-end', '-start')
+            return queryset.filter(private=False).distinct().order_by('-end', '-start')
+
+
+class EventInviteView(generics.RetrieveUpdateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = InviteEventSerializer
+    lookup_field = 'uuid'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = EventSerializer(instance, context={'request':request})
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        serializer = EventSerializer(instance, context={'request': request})
+        return Response(serializer.data)
